ca47dbdb69a40172bbb11d487ead1abe2d2072b4
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 18:11:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,19 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructielyAddElementToEndOfArray(array, element){
+  return array.push(element)
 }
