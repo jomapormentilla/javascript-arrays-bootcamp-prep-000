f07ccdc405258b24bb2c95ddf8d689c92118884c
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 18:18:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element){
@@ -20,5 +21,6 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructielyAddElementToEndOfArray(array, element){
-  return array.push(element)
+  array.push(element)
+  return array
 }
